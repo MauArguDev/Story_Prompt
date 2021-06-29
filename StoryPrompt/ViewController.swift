800afbbc2cc7b5bfd2bd10d33d035745905d72f2
Mauricio Hernandez <mauricio.hernandez@devsu.com>
2021-06-29 17:15:10 -0500

    Initial Commit

---
+++ StoryPrompt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by user199344 on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
